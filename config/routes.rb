2ff3c30a6bb440b7cae1e7b482bb396cbd7b158d
Willem van Klinken <willem.v.klinken@gmail.com>
2017-11-07 14:26:24 +0100

    task model controller and views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+#read all
+  get 'tasks', to: 'tasks#index'
+
+#create (one)
+  get    "tasks/new",      to: "tasks#new", as:'new_task'
+  post   "tasks",          to: "tasks#create"
+
+#read one
+  get 'tasks/:id', to: 'tasks#show', as:'task'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#update a task
+  get    "tasks/:id/edit", to: "tasks#edit" , as: "edit_task"
+  patch  "tasks/:id",      to: "tasks#update"
+
+#delete one
+  delete "tasks/:id",      to: "tasks#destroy"
 end
